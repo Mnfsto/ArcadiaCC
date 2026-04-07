4cde3f83dc79688e2f0a24eec87325c71d001544
Airmax <titov.maxim@icloud.com>
2026-04-07 21:56:49 +0300

    fix nodemailer

--- server/mailer/nodemailer.js
+++ server/mailer/nodemailer.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
+const hbsModule = require('nodemailer-express-handlebars');
+const hbs = hbsModule.default || hbsModule;
 const path = require('path');
 const smtp = require('./config');
 
